0c961d5a03edc38efd66f60213756f7e64dd451e
aallm004 <abigailrosea10@gmail.com>
2024-05-30 21:24:19 +0000

    added def save to Base

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,31 @@ class BaseModel():
 #!/usr/bin/python3
 """Base model class for AirBnB - The Console
 """
+import uuid
+from datetime import datetime
 
 
 class Base:
     """Base Model Class"""
-    def __init__(self):
+    
+    def __init__(self, *args, **kwargs):
         """Base Model Class Initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+                else:
+                    
+    def save(self):
+        """Update updated_at with current datetime."""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+
+
+
 
 
