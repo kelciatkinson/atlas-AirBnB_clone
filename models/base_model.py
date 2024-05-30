18804792003aad35cea69f869c01a216cf55c9ab
aallm004 <abigailrosea10@gmail.com>
2024-05-30 20:04:23 +0000

    Changes to Base Model Class

--- models/base_model.py
+++ models/base_model.py
@@ -6,3 +6,18 @@ class BaseModel():
     def __init__(self, id=None):
         self.id = uuid.uuid4()
 
+#!/usr/bin/python3
+"""Base model class for AirBnB - The Console
+"""
+
+
+class Base:
+    """Base Model Class"""
+    def __init__(self):
+        """Base Model Class Initialization"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+
+
