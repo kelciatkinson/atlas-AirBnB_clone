00f78b48cc153466b5116d133153decec347f5ed
aallm004 <abigailrosea10@gmail.com>
2024-05-31 19:39:04 +0000

    test for test_save

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,15 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = self.updated_at = datetime.now()
 
     def to_dict(self):
         dictionary = self.__dict__.copy()
