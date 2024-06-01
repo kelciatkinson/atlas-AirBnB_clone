bbdd042a58b0b1666b7104a1a5aac4965b70005b
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-01 15:02:15 -0500

    added class File Storage with all and new methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,4 +7,33 @@ from models.base_model import BaseModel
 
 class FileStorage():
     """File Storage Class"""
-    
+    def __init__(self, file_path, objects):
+        self.file_path = file_path
+        # string (path to the JSON file (ex: file.json))
+        self.objects = objects
+        # dictionary (empty byt will store all objects by <class name>.id
+        # (ex: to store a BaseModel object with id=12121212, the key will be BaseMode.12121212))
+
+    @property
+    def file_path(self):
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self, value):
+        self.__file_path = value
+
+    @property
+    def objects(self):
+        return self.__objects
+
+    @objects.setter
+    def objects(self, value):
+
+        self.__objects = value
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.objects[key] = obj
