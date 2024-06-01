e59b41139cdbc50ca27a54625a85c7244d63a3de
“kelciatkinson” <kelciatkinson@gmail.com>
2024-05-31 19:55:54 -0500

    created class FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the class FileStorage"""
+import json
+from models.base_model import BaseModel
+# from models import storage
+
+
+class FileStorage():
+    """File Storage Class"""
+    
