f8422b2fc5e0ac1b75d95ef1470f9c9c48b3a45d
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-03 12:06:04 -0500

    added test for file_path and moved test file into tests/test_engine

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,13 +6,16 @@ from models.base_model import BaseModel
 
 
 
-class Test_FileStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     def test_init(self):
         """was a new FileStorage created"""
         file = FileStorage()
         self.assertIsInstance(file, FileStorage)
 
     def test_file_path(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_file_path_exists(self):
         """Tests an instance of FileStorage to see if the private
         "file_path" attribute is "file.json"."""
         self.assertTrue(os.path.exists("file.json"))
