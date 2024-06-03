f4efd0adbfc6b0eadf67c1b2701f734c2254b153
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-03 12:43:51 -0500

    added test for objects, moved files

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,8 +1,8 @@
 """test_file_storage"""
 import unittest
 import os
+import models
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
 
 
 
@@ -14,6 +14,11 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file_path(self):
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_objects(self):
+        """Tests an instance of FileStorage to see if the private
+        "file_path" attribute is "file.json"."""
+        self.assertEqual(dict, type(models.storage.all))
 
 if __name__ == "__main__":
     unittest.main()
