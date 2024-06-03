d765d328a3d583b2ab26cd5d577eec67fbddd1f9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-03 12:45:05 -0500

    removed test for models

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -15,10 +15,5 @@ class TestFileStorage(unittest.TestCase):
     def test_file_path(self):
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-    def test_objects(self):
-        """Tests an instance of FileStorage to see if the private
-        "file_path" attribute is "file.json"."""
-        self.assertEqual(dict, type(models.storage.all))
-
 if __name__ == "__main__":
     unittest.main()
