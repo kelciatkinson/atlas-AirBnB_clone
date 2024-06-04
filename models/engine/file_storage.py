5174ab431ccb4dde1d51afdc30efc4549addf8a6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-06-04 17:20:44 -0500

    cleaned up file and documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,33 +14,30 @@ from models.review import Review
 class FileStorage():
     """File Storage Class to manage serialization
     and deserialization of objects to and from a JSON file"""
+
+    # Private attributes 
     __file_path = "file.json"
     __objects = {}
-
-    @property
-    def file_path(self):
-        return self.__file_path
-
-    @file_path.setter
-    def file_path(self, value):
-        self.__file_path = value
 
     def all(self):
         """Returns the dictionary __objects.
         This method provides access to the dictionary "__objects" that
         stores all objects managed by the instance "FileStorage"."""
+
         return self.__objects
 
     def new(self, obj):
-        """This method adds a new object to the __objects dictionary. The key
-        for the object is the class name with object's ID separated by a period.
-        The object is the value."""
+        """This method adds a new object to the __objects dictionary. 
+        The object's key isthe class name with object's ID separated
+        by a period. The object is the value."""
+
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """This method serializes __objects to the JSON file path:
-        "__file_path"."""
+        """This method serializes __objects to the JSON file path
+        located in "__file_path" variable."""
+
         objects_dict = {}
         with open(self.__file_path, 'w') as file:
             for key, value in self.__objects.items():
@@ -48,9 +45,9 @@ class FileStorage():
             json.dump(objects_dict, file)
 
     def reload(self):
-        """This method deserializes the JSON file specified by "__file_path"
-        and populates the "__objects" dictionary with objects stored
-        in the JSON file."""
+        """This method deserializes the JSON file and populates the
+        "__objects" dictionary with objects stored in the JSON file."""
+
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, "r") as file:
                 objects_dict = json.load(file)
